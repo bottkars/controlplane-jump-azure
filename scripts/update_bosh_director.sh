6cc905b8047e5b4828924776de2bc0c12d9f02e1
bottkars <karsten.bott@bottnet.de>
2019-07-17 15:15:52 +0200

    latest

--- scripts/update_bosh_director.sh
+++ scripts/update_bosh_director.sh
@@ -41,15 +41,15 @@ AZURE_CLIENT_ID=$(curl https://${AZURE_VAULT}.vault.azure.net/secrets/AZURECLIEN
 AZURE_TENANT_ID=$(curl https://${AZURE_VAULT}.vault.azure.net/secrets/AZURETENANTID?api-version=2016-10-01 -s -H "Authorization: Bearer ${TOKEN}" | jq -r .value)
 PIVNET_UAA_TOKEN=$(curl https://${AZURE_VAULT}.vault.azure.net/secrets/PIVNETUAATOKEN?api-version=2016-10-01 -H "Authorization: Bearer ${TOKEN}" | jq -r .value)
 
-BOSH_STORAGE_ACCOUNT_NAME=$(terraform output -state ~/pivotal-cf-terraforming-azure-*/terraforming-control-plane/terraform.tfstate ops_manager_storage_account)
+OPS_MANAGER_STORAGE_ACCOUNT=$(terraform output -state ~/pivotal-cf-terraforming-azure-*/terraforming-control-plane/terraform.tfstate ops_manager_storage_account)
 
 export AZURE_STORAGE_CONNECTION_STRING=$(az storage account show-connection-string \
---name ${BOSH_STORAGE_ACCOUNT_NAME} --resource-group ${ENV_NAME})
+--name ${OPS_MANAGER_STORAGE_ACCOUNT} --resource-group ${ENV_NAME})
 export OPSMAN_IMAGE_VERSION=$(grep -A1 'OPSMAN_IMAGE:' ${ENV_DIR}/opsman.yml | tail -n1 | cut -c15- )
 
 export OPSMAN_IMAGE_URI=$(dirname ${OPS_MANAGER_IMAGE_URI})/ops-manager-${OPSMAN_IMAGE_VERSION}.vhd
 
-AZURE_STORAGE_ENDPOINT=$(az storage account show --name ${BOSH_STORAGE_ACCOUNT_NAME} \
+AZURE_STORAGE_ENDPOINT=$(az storage account show --name ${OPS_MANAGER_STORAGE_ACCOUNT} \
  --resource-group ${ENV_NAME} \
   --query '[primaryEndpoints.blob]' --output tsv)
 OPSMAN_LOCAL_IMAGE=${AZURE_STORAGE_ENDPOINT}opsmanagerimage/opsman-image-${OPSMAN_IMAGE_VERSION}.vhd
