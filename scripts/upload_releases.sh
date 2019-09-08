5d4675c6d14ae2c2a03d324da9d01ff806331761
bottkars <karsten.bott@bottnet.de>
2019-09-08 07:29:04 +0200

    latest uaa

--- scripts/upload_releases.sh
+++ scripts/upload_releases.sh
@@ -3,7 +3,6 @@ source ~/.env.sh
 cd ${HOME_DIR}
 
 bosh upload-release https://bosh.io/d/github.com/concourse/concourse-bosh-release?v=5.5.0
-bosh upload-release https://bosh.io/d/github.com/cloudfoundry/uaa-release?v=69.0
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry/bpm-release?v=1.1.3
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry/bosh-dns-aliases-release?v=0.0.3
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry/garden-runc-release?v=1.19.3
@@ -15,6 +14,7 @@ bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/windows-
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry/windowsfs-online-release?v=1.7.0
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/winc-release?v=1.14.0
 bosh upload-release https://bosh.io/d/github.com/cloudfoundry-incubator/garden-windows-bosh-release?v=0.16.0
+
 bosh deploy -n -d control-plane ${HOME_DIR}/conductor/templates/control-plane-deployment-kb-5.yml \
    --vars-file=${HOME_DIR}/bosh-vars.yml \
    --ops-file=${HOME_DIR}/vm-extensions-control.yml \
